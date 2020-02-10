cd38bddb5a62a12dc3ffe89fd6cc7dca935ee938
uedi <uedi@users.noreply.github.com>
2020-02-10 20:57:04 +0200

    backend with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.static('build'))
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
 morgan.token('postdata', function (request, response) {
     if(request.method === 'POST') {
@@ -17,85 +19,90 @@ morgan.token('postdata', function (request, response) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'))
 
-let persons = [
-    { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-    },
-    { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-    },
-    { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+    .then(persons => {
+        response.json(persons.map(person => person.toJSON()))
+    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    const count = persons.length
-    const date = new Date()
-    response.send(`<div>
+app.get('/info', (request, response, next) => {
+    Person.count()
+    .then(count => {
+        const date = new Date()
+        response.send(`<div>
                         <p>Phonebook has info for ${count} people</p>
                         <p>${date}</p>
                     </div>`)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if(person) {
+            response.json(person.toJSON())
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(results => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const getNewId = () => {
-    let id = Math.floor(1 + Math.random() * 30000)
-    while(persons.find(person => person.id === id)) {
-        id = Math.floor(1 + Math.random() * 30000)
-    }
-    return id;
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if(!body.name || !body.number) {
         return response.status(400).json({error: 'name or number missing'})
-    } else if(persons.find(person => person.name === body.name)) {
-        return response.status(400).json({error: 'name must be unique'})
-    }
-
-    const person = {
+    } 
+    
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: getNewId()
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
+        number: body.number
+    })
+    
+    person.save()
+    .then(savedPerson => {
+        response.json(savedPerson.toJSON())
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if(error.name === 'CastError' && error.kind === 'ObjectId') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if(error.name === 'ValidationError') {
+        return response.status(400).send({error: error.message})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if( process.argv.length < 3 ) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0-xiqa3.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const getPersons = () => {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+const addPerson = (name, number) => {
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    const person = new Person({
+        name: name,
+        number: number
+    })
+    person.save().then(response => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+if(process.argv.length === 3) {
+    getPersons()
+} else if(process.argv.length === 5) {
+    addPerson(process.argv[3], process.argv[4])
+} else {
+    console.log("invalid number of arguments")
+}
